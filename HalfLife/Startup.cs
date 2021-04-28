1c079db0ede6af9bd1b586ef243aa2522b02ba07
gawronCode <gawroncode.gmail>
2021-04-28 22:57:20 +0200

    test Commit

--- HalfLife/Startup.cs
+++ HalfLife/Startup.cs
@@ -76,6 +76,7 @@ namespace HalfLife
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+               // dasd 
             });
         }
     }
