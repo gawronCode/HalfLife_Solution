1d0d11621db9fef745f452119250954e95c6eb24
gawronCode <gawroncode.gmail>
2021-04-28 22:39:44 +0200

    Add project files.

---
+++ HalfLife/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HalfLife.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
