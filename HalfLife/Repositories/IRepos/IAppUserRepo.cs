de4cb8b154065908885ce52b04e5c7d7c7b3ab05
gawronCode <gawroncode@gmail.com>
2021-04-29 19:02:07 +0200

    Add DbModels interface repositories

---
+++ HalfLife/Repositories/IRepos/IAppUserRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HalfLife.Models.DbModels;
+
+namespace HalfLife.Repositories.IRepos
+{
+    interface IAppUserRepo : IGeneralRepo<AppUser>
+    {
+    }
+}
---
+++ HalfLife/Repositories/IRepos/IGeneralRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfLife.Repositories.IRepos
+{
+    interface IGeneralRepo<T> where T : class
+    {
+        Task<T> GetById(int id);
+        Task<bool> Create(T entity);
+        Task<bool> Delete(T entity);
+        Task<bool> Save();
+    }
+}
